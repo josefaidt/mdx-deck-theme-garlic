a84368cfc3b291e46d0425d1ffe0a0dc7b2c46cd
josefaidt <josef.aidt@gmail.com>
2019-08-13 15:44:29 -0500

    add builder

--- theme/index.js
+++
@@ -1,7 +0,0 @@
-import theme from './src/theme'
-export { default as Provider } from './src/components/Provider'
-export { default as CodeSandbox } from './src/components/CodeSandbox'
-export { default as Frame } from './src/components/Frame'
-export { default as Contour } from './src/layouts/contour'
-export { default as GithubIcon } from './src/components/GithubIcon'
-export default theme
