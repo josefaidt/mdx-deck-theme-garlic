5a4740fb0c3ddae1d3e1e12587d8ce76c531b522
josef <josef.aidt@gmail.com>
2019-09-15 20:18:23 -0500

    cleanup, add contour framed

--- theme/src/index.js
+++ theme/src/index.js
@@ -2,6 +2,6 @@ import theme from './theme'
 export { default as Provider } from './components/Provider'
 export { default as CodeSandbox } from './components/CodeSandbox'
 export { default as Frame } from './components/Frame'
-export { default as Contour } from './layouts/contour'
+export { default as Contour } from './layouts/Contour'
 export { default as GithubIcon } from './components/GithubIcon'
 export default theme
